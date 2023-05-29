578c7ef3a3f704e97b1da3a899ed7cdf8a7c1ca4
a22zacno <a22zano@studnet.his.se>
2023-05-29 10:38:36 +0200

    Added some pref variables and a method to start a second activity.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,26 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView SharedText;
+
+    private static final String SHARED_PREF_NAME = "sharedPref";
+    private static final String KEY_TEXT = "keyText";
+
+    private String shared;
+    public void startEdit(View view) {
+        //Starts the second activity
+        Intent i = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +29,4 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
-
 }
