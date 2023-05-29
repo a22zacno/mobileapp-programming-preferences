7b8acf210d62b30fffd9613d9cc1b70eb3561182
a22zacno <a22zano@studnet.his.se>
2023-05-29 10:51:10 +0200

    Writes the text in textEditor to the shared preferences

--- app/src/main/java/com/example/preferences/EditActivity.java
+++ app/src/main/java/com/example/preferences/EditActivity.java
@@ -3,12 +3,26 @@ package com.example.preferences;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class EditActivity extends AppCompatActivity {
 
+    private EditText textEditor;
+
+    private static final String SHARED_PREF_NAME = "sharedPref";
+    private static final String KEY_TEXT = "keyText";
+
     public void startMain(View view) {
+        textEditor = (EditText) findViewById(R.id.textEditor);
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(KEY_TEXT,textEditor.getText().toString());
+        editor.commit();
+
+        finish();
     }
 
     @Override
